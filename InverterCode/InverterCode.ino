d85cda9327807733d084c527b8842381fc907f5c
Matthew5965 <matthewappleby5965@gmail.com>
2021-10-19 19:39:33 +0100

    Code doesn't run

--- InverterCode/InverterCode.ino
+++ InverterCode/InverterCode.ino
@@ -8,10 +8,10 @@ void setup() {
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
   
-  frequency=1;  //Hz
-  period=1/frequency; //s
-  switchingPeriod=0.05 //s
-  float LineOneHigh={0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45};
+  int frequency=1;  //Hz
+  float period=1/frequency; //s
+  float switchingPeriod=0.05; //s
+  float LineHigh={0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45};
   float LineOneLow={0.0, 0.065, 0.129, 0.19, 0.248, 0.3, 0.348, 0.39, 0.429, 0.466};
   //LineTwoHigh=[]
   //LineTwoLow=[]
@@ -22,14 +22,14 @@ void setup() {
 void loop() {
   for (int i = 0; i <= period/switchingPeriod; i++) {
     pinMode(1,HIGH);
-    float wait = LineOneLow[i]-LineOneHigh[i];
+    float wait = LineOneLow[i]-LineHigh[i];
     delay(wait);//millis
     pinMode(1,LOW);
-    delay(switchingPeriod-wait)
+    delay(switchingPeriod-wait);
   }
   for (int i = 0; i <= period/switchingPeriod; i++) {
     pinMode(2,HIGH);
-    float wait = LineOneLow[i]-LineOneHigh[i];
+    float wait = LineOneLow[i]-LineHigh[i];
     delay(wait);//millis
     pinMode(2,LOW);
     delay(switchingPeriod-wait)
