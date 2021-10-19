2a46a0aac4b550ac1a2183b9eb2918599eafd06f
Matthew5965 <matthewappleby5965@gmail.com>
2021-10-19 18:57:15 +0100

    Works for 1 phase half cycle

--- LookupTable.py
+++ LookupTable.py
@@ -1,6 +1,13 @@
 """
 Build look-up table for 360 degrees
 Conceptually easier and faster to run
+
+local: wind
+
+todo:
+--output a pwm wave that actually works
+--Do that for all three phases both positive and negative parts
+
 """
 import math
 import seaborn as sns
@@ -8,9 +15,9 @@ import matplotlib.pyplot as plt
 
 frequency=1 #Hz
 period=1/frequency #s
-switchingPeriod=0.01 #s
+switchingPeriod=0.05 #s
 
-scalingConstant=0.1
+scalingConstant=1#0.1
 phaseShift=0
 
 LineOneHigh=[]
@@ -23,24 +30,29 @@ LineThreeLow=[]
 LineOne=[]
 LineOneStates=[]
 
-for x in range(int(period/switchingPeriod)):
-    time=x*switchingPeriod
-    Ton=scalingConstant*math.sin((2*(3.14)/period)*time+phaseShift)
-    Toff=period-Ton
+for cycle in range(int((int(period/switchingPeriod)+2)/2)):#iterates throught number of cycles
+    time=cycle*switchingPeriod
+    TurnOn=switchingPeriod*math.sin((2*(3.14)/period)*time+phaseShift)
+    #TurnOff=period-TurnOn
+    
+    #turn on is turn on length
+
+    
     LineOneHigh.append(round(time,3))
-    LineOneLow.append(round((time+Ton),3))
+    LineOneLow.append(round((time+TurnOn),3))
 
     LineOne.append(round(time,3))
-    LineOne.append(round((time+Ton),3))
+    LineOne.append(round((time+TurnOn),3))
 
     LineOneStates.append(1)
-    LineOneStates.append(0)
+    #LineOneStates.append(0)
 
-
+print(LineOneHigh)
 print(LineOneLow)
 
 
-#sns.lineplot(data=[LineOne,LineOneStates], x=LineOne, y=LineOneStates, color="black", marker="o")
-plt.plot(LineOne, LineOneStates)
+plt.scatter(LineOneLow, LineOneStates, c='y')
+plt.scatter(LineOneHigh, LineOneStates, c='b')
 plt.show()
-#plt.title("Output", fontsize=20)
+
+#plt.plot(LineOne,1)
